eeb26e21e499318185e8586ea7ccbcd60f0e55d5
danielgp <danielgarduno282@gmail.com>
2025-08-02 22:26:01 -0600

    Proyecto 3

--- Proyecto1-Tarjeta_presentacion/mi-tarjeta/src/App.jsx
+++ Proyecto1-Tarjeta_presentacion/mi-tarjeta/src/App.jsx
@@ -1,23 +1,65 @@
-import Tarjeta from './Tarjeta';
-import ListaCompras from './Listacompras';
+import React, { useState, useEffect, useMemo } from 'react';
+import "./Compras.css";
+
 function App() {
+  const [tareas, setTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState('');
+  const [duracion, setDuracion] = useState('');
+
+  const calcularTiempoTotal = useMemo(() => {
+    console.log("Calculando tiempo total...");
+    return tareas.reduce((total, tarea) => total + tarea.duracion, 0);
+  }, [tareas]); // Solo se recalcula cuando cambian las tareas
+
+  // Efecto secundario: Actualizar el título del documento cada vez que cambia el total
+  useEffect(() => {
+    // Usamos el valor directamente de totalTiempoCalculado
+    document.title = `Total: ${calcularTiempoTotal} minutos`;
+  }, [calcularTiempoTotal]); // Se ejecuta cada vez que el totalTiempoCalculado cambia
+
+  // Función para agregar una nueva tarea
+  const agregarTarea = (e) => {
+    e.preventDefault(); // Previene la recarga de la página
+
+    if (nuevaTarea.trim() !== '' && duracion.trim() !== '') {
+      const nuevaTareaObj = {
+        nombre: nuevaTarea,
+        duracion: parseInt(duracion) // Asegúrate de que la duración sea un número
+      };
+      setTareas([...tareas, nuevaTareaObj]);
+      setNuevaTarea('');
+      setDuracion('');
+    }
+  };
+
+
   return (
     <div>
-      <h1>Tarjeta de Presentación</h1>
-      {/* Renderizamos el componente Tarjeta */}
-      <Tarjeta 
-        nombre={"Carlos"}
-        profesion={"Ingeniero en telecomunicaciones"}
-      />
-      <Tarjeta 
-        nombre={"Daniel"}
-        profesion={"Ingeniero aeroespacial"}
-      />
-      <Tarjeta 
-        nombre={"Alexis"}
-        profesion={"Ingeniero civil"}
-      />
-      <ListaCompras/>
+      <h1>Contador de Tareas</h1>
+      <div>
+        <input 
+          type="text" 
+          value={nuevaTarea} 
+          onChange={(e) => setNuevaTarea(e.target.value)} 
+          placeholder="Nombre de la tarea" 
+        />
+        <input 
+          type="number" 
+          value={duracion} 
+          onChange={(e) => setDuracion(e.target.value)} 
+          placeholder="Duración en minutos" 
+        />
+        <button onClick={agregarTarea}>Agregar tarea</button>
+      </div>
+
+      <h2>Tareas</h2>
+      <ul>
+        {tareas.map((tarea, index) => (
+          <li key={index}>{tarea.nombre}: {tarea.duracion} minutos</li>
+        ))}
+      </ul>
+
+      <h3>Total de tiempo: {calcularTiempoTotal} minutos</h3>
     </div>
   );
 }
